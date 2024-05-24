ffd381e7b539d44d625abcadc20686ea1578868b
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-05-24 14:20:51 +0100

    Testing authentication handler

---
+++ authentication-service/cmd/api/handlers_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_Authenticate(t *testing.T) {
+	postBody := map[string]interface{}{
+		"email":    "me@here.com",
+		"password": "verysecret",
+	}
+
+	body, _ := json.Marshal(postBody)
+
+	req, _ := http.NewRequest("POST", "/authenticate", bytes.NewReader(body))
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(testApp.Authenticate)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusAccepted {
+		t.Errorf("expected http.StatusAcceptedbut got %d", rr.Code)
+	}
+}
